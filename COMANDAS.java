49f9a89f917f9a8d86e9ca868aed5ea00ab16ca5
Saulo Verissimo <103527377+sveriuniceo@users.noreply.github.com>
2023-11-27 10:31:06 -0300

    Add files via upload
    Sistema simples de comanda eletrônica em java

---
+++ COMANDAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.comandas;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class COMANDAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
